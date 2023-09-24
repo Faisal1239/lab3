e95947ae61d6e5406d93f1e732ef6bce98a7e72d
AbdulrhmanAmmar20 <s202183170@kfupm.edu.sa>
2023-09-24 19:45:00 +0300

    resolve the conflict

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("the conflict statment from the first computer");
+
+        System.out.println("THE CONFLICT STATEMENT FROM THE SECOND COMPUTER");
+    }}
